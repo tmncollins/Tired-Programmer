0e01ed8446ef8a8a8dac4012958ebefd2e71dfdb
tmncollins <35235207+tmncollins@users.noreply.github.com>
2021-04-04 17:10:51 +0100

    Add files via upload

---
+++ problem_handler.js
@@ -0,0 +1,24 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    var allText = "";
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
+              
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+function loadProblems(file) {
+
+}
+
